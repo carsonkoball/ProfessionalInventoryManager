718506c78842fbcd154dd6571dd857085d7fb82f
Carson Koball <Carson.Koball@trojans.dsu.edu>
2021-11-11 20:57:04 -0600

    Initial commit

---
+++ Code/Professional Inventory Manager/Form1.cs
@@ -0,0 +1,10 @@
+namespace Professional_Inventory_Manager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Code/Professional Inventory Manager/Program.cs
@@ -0,0 +1,15 @@
+namespace Professional_Inventory_Manager
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
