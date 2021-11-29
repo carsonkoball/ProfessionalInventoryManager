9e12c254a6a4ae44038187e26d537747c27f3877
JosieJJ <45248200+JosieJJ@users.noreply.github.com>
2021-11-28 22:56:30 -0600

    main with table display and buttons working
    still need logout button and first data table

--- Professional Inventory Manager/Professional Inventory Manager/Main.Designer.cs
+++ Professional Inventory Manager/Professional Inventory Manager/Main.Designer.cs
@@ -32,9 +32,9 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.Column11 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
             this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -51,8 +51,11 @@
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.textBox5 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
             this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -62,9 +65,10 @@
             this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Location = new System.Drawing.Point(630, 517);
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(254, 595);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(91, 30);
+            this.button1.Size = new System.Drawing.Size(91, 23);
             this.button1.TabIndex = 7;
             this.button1.Text = "Add Item";
             this.button1.UseVisualStyleBackColor = false;
@@ -75,37 +79,42 @@
             this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
             this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.button2.Location = new System.Drawing.Point(628, 555);
+            this.button2.Location = new System.Drawing.Point(351, 595);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(93, 30);
+            this.button2.Size = new System.Drawing.Size(93, 23);
             this.button2.TabIndex = 1;
             this.button2.Text = "Update Item";
             this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
             this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
             this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button3.Location = new System.Drawing.Point(628, 591);
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button3.Location = new System.Drawing.Point(450, 595);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(93, 27);
+            this.button3.Size = new System.Drawing.Size(93, 23);
             this.button3.TabIndex = 2;
             this.button3.Text = "Delete Item";
             this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
-            // dataGridView2
+            // dataGridView1
             // 
-            this.dataGridView2.AllowUserToAddRows = false;
-            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Column11});
-            this.dataGridView2.Location = new System.Drawing.Point(14, 92);
-            this.dataGridView2.Name = "dataGridView2";
-            this.dataGridView2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.dataGridView2.Size = new System.Drawing.Size(205, 384);
-            this.dataGridView2.TabIndex = 4;
+            this.dataGridView1.Location = new System.Drawing.Point(14, 92);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(205, 426);
+            this.dataGridView1.TabIndex = 4;
             // 
             // Column11
             // 
@@ -113,22 +122,25 @@
             this.Column11.HeaderText = "Inventory Space Name";
             this.Column11.Name = "Column11";
             // 
-            // dataGridView3
+            // dataGridView2
             // 
-            this.dataGridView3.AllowUserToAddRows = false;
-            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView3.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Column6,
             this.Column7,
             this.Column8,
             this.Column9,
             this.Column10});
-            this.dataGridView3.Location = new System.Drawing.Point(234, 92);
-            this.dataGridView3.Name = "dataGridView3";
-            this.dataGridView3.RowTemplate.Height = 25;
-            this.dataGridView3.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataGridView3.Size = new System.Drawing.Size(554, 384);
-            this.dataGridView3.TabIndex = 5;
+            this.dataGridView2.Location = new System.Drawing.Point(234, 92);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 25;
+            this.dataGridView2.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView2.ShowEditingIcon = false;
+            this.dataGridView2.Size = new System.Drawing.Size(554, 384);
+            this.dataGridView2.TabIndex = 5;
+            this.dataGridView2.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.dataGridView2_MouseDoubleClick);
             // 
             // Column6
             // 
@@ -138,28 +150,32 @@
             // 
             // Column7
             // 
+            this.Column7.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
             this.Column7.HeaderText = "Name";
             this.Column7.Name = "Column7";
             // 
             // Column8
             // 
+            this.Column8.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
             this.Column8.HeaderText = "Quantity";
             this.Column8.Name = "Column8";
             // 
             // Column9
             // 
+            this.Column9.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
             this.Column9.HeaderText = "Price";
             this.Column9.Name = "Column9";
             // 
             // Column10
             // 
+            this.Column10.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
             this.Column10.HeaderText = "Shipping Date";
             this.Column10.Name = "Column10";
             // 
             // textBox2
             // 
             this.textBox2.ForeColor = System.Drawing.Color.Gray;
-            this.textBox2.Location = new System.Drawing.Point(12, 519);
+            this.textBox2.Location = new System.Drawing.Point(14, 566);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(148, 23);
             this.textBox2.TabIndex = 6;
@@ -177,7 +193,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(234, 63);
+            this.label1.Location = new System.Drawing.Point(234, 64);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(72, 15);
             this.label1.TabIndex = 8;
@@ -225,7 +241,7 @@
             // textBox3
             // 
             this.textBox3.ForeColor = System.Drawing.Color.Gray;
-            this.textBox3.Location = new System.Drawing.Point(166, 519);
+            this.textBox3.Location = new System.Drawing.Point(168, 566);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(148, 23);
             this.textBox3.TabIndex = 10;
@@ -234,7 +250,7 @@
             // textBox4
             // 
             this.textBox4.ForeColor = System.Drawing.Color.Gray;
-            this.textBox4.Location = new System.Drawing.Point(320, 519);
+            this.textBox4.Location = new System.Drawing.Point(322, 566);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(148, 23);
             this.textBox4.TabIndex = 11;
@@ -243,7 +259,7 @@
             // textBox5
             // 
             this.textBox5.ForeColor = System.Drawing.Color.Gray;
-            this.textBox5.Location = new System.Drawing.Point(474, 519);
+            this.textBox5.Location = new System.Drawing.Point(476, 566);
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(148, 23);
             this.textBox5.TabIndex = 12;
@@ -252,17 +268,53 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 493);
+            this.label2.Location = new System.Drawing.Point(14, 548);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(216, 15);
             this.label2.TabIndex = 13;
             this.label2.Text = "Add New Item Or Change Current Item:";
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button4.Location = new System.Drawing.Point(585, 60);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(93, 23);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "Search";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox6
+            // 
+            this.textBox6.ForeColor = System.Drawing.Color.Gray;
+            this.textBox6.Location = new System.Drawing.Point(630, 566);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(148, 23);
+            this.textBox6.TabIndex = 15;
+            this.textBox6.Text = "Shipping Date...";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(234, 492);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(524, 15);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Double-click row header to select an item. The item information will be shown in " +
+    "the boxes below.";
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 630);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox5);
             this.Controls.Add(this.textBox4);
@@ -271,8 +323,8 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.dataGridView3);
             this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -280,8 +332,8 @@
             this.Text = "Professional Inventory Manager";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Main_FormClosing);
             this.Load += new System.EventHandler(this.Main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
             this.toolStrip1.ResumeLayout(false);
             this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
@@ -294,13 +346,8 @@
         private Button button1;
         private Button button2;
         private Button button3;
+        private DataGridView dataGridView1;
         private DataGridView dataGridView2;
-        private DataGridView dataGridView3;
-        private DataGridViewTextBoxColumn Column6;
-        private DataGridViewTextBoxColumn Column7;
-        private DataGridViewTextBoxColumn Column8;
-        private DataGridViewTextBoxColumn Column9;
-        private DataGridViewTextBoxColumn Column10;
         private TextBox textBox2;
         private TextBox textBox1;
         private Label label1;
@@ -313,5 +360,13 @@
         private TextBox textBox4;
         private TextBox textBox5;
         private Label label2;
+        private DataGridViewTextBoxColumn Column6;
+        private DataGridViewTextBoxColumn Column7;
+        private DataGridViewTextBoxColumn Column8;
+        private DataGridViewTextBoxColumn Column9;
+        private DataGridViewTextBoxColumn Column10;
+        private Button button4;
+        private TextBox textBox6;
+        private Label label3;
     }
 }
--- Professional Inventory Manager/Professional Inventory Manager/Main.cs
+++ Professional Inventory Manager/Professional Inventory Manager/Main.cs
@@ -27,29 +27,157 @@ namespace Professional_Inventory_Manager
             using (con = new SqlConnection(connectionString))
             {
                 con.Open();
-                SqlCommand cmd = new SqlCommand(@"INSERT INTO [dbo].[Item]
-                ([ItemID]
-                ,[ItemName]
-                ,[ItemQuantity]
-                ,[ItemPrice])
-                VALUES
-                ('" + textBox2.Text +"','"+ textBox3.Text +"','"+ textBox4.Text+ "','"+ textBox5.Text +"')", con);
-                cmd.ExecuteNonQuery();
 
+                SqlCommand cmd = new SqlCommand(@"INSERT INTO[dbo].[Item]
+                        ([ItemID]
+                        ,[ItemName]
+                        ,[ItemQuantity]
+                       ,[ItemPrice]
+                       ,[ItemShippingDate])
+                        VALUES
+                       ('" + textBox2.Text + "', '" + textBox3.Text + "', '" + textBox4.Text + "', '" + textBox5.Text + "', '" + textBox6.Text + "')", con);
+                cmd.ExecuteNonQuery();
                 con.Close();
 
+                //read in table data
+                LoadData();
             }
         }
 
-        //closes the program when "x" button is used
+        private bool IfItemExists(SqlConnection con, string itemID)
+        {
+            //checks if an item exists in the system
+            string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""C:\Users\Dingo\source\repos\ProfessionalInventoryManager\Professional Inventory Manager\Professional Inventory Manager\DB\Data.mdf"";Integrated Security=True;Connect Timeout=30;";
+            using (con = new SqlConnection(connectionString))
+            {
+                SqlDataAdapter sda = new SqlDataAdapter("Select 1 From [Item] WHERE [ItemID] = '" + itemID + "'", con); //Select 1 checks if it's there or not
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+                if (dt.Rows.Count > 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        public void LoadData()
+        {
+            //a function that reads data from the table into the grid (Grid2)
+            string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""C:\Users\Dingo\source\repos\ProfessionalInventoryManager\Professional Inventory Manager\Professional Inventory Manager\DB\Data.mdf"";Integrated Security=True;Connect Timeout=30;";
+            SqlConnection con;
+            using (con = new SqlConnection(connectionString))
+            {
+                SqlDataAdapter sda = new SqlDataAdapter("Select * From [dbo].[Item]", con);
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+                dataGridView2.Rows.Clear();
+
+                foreach (DataRow item in dt.Rows)
+                {
+                    int n = dataGridView2.Rows.Add();
+                    dataGridView2.Rows[n].Cells[0].Value = item["ItemID"].ToString();
+                    dataGridView2.Rows[n].Cells[1].Value = item["ItemName"].ToString();
+                    dataGridView2.Rows[n].Cells[2].Value = item["ItemQuantity"].ToString();
+                    dataGridView2.Rows[n].Cells[3].Value = item["ItemPrice"].ToString();
+                    dataGridView2.Rows[n].Cells[4].Value = item["ItemShippingDate"].ToString();
+                }
+            }
+        }
+
         private void Main_FormClosing(object sender, FormClosingEventArgs e)
         {
+            //closes the program when "x" button is used
             Application.Exit();
         }
 
         private void Main_Load(object sender, EventArgs e)
         {
+            //loads item data into the table once main loads
+            LoadData();
+        }
 
+        private void dataGridView2_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            //double-clicking the row header will put selected item info into the text boxes
+            textBox2.Text = dataGridView2.SelectedRows[0].Cells[0].Value.ToString();
+            textBox3.Text = dataGridView2.SelectedRows[0].Cells[1].Value.ToString();
+            textBox4.Text = dataGridView2.SelectedRows[0].Cells[2].Value.ToString();
+            textBox5.Text = dataGridView2.SelectedRows[0].Cells[3].Value.ToString();
+            textBox6.Text = dataGridView2.SelectedRows[0].Cells[4].Value.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //delete selected item from the table
+            string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""C:\Users\Dingo\source\repos\ProfessionalInventoryManager\Professional Inventory Manager\Professional Inventory Manager\DB\Data.mdf"";Integrated Security=True;Connect Timeout=30;";
+            SqlConnection con;
+            using (con = new SqlConnection(connectionString))
+            {
+                //check if item record exists
+                var sqlQuery = "";
+
+                //delete items
+                if (IfItemExists(con, textBox2.Text))
+                {
+                    con.Open();
+                    //this item does exist, delete
+                    sqlQuery = @"DELETE FROM [Item] WHERE [ItemID] = '" + textBox2.Text + "'";
+                    SqlCommand cmd = new SqlCommand(sqlQuery, con);
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+                else
+                {
+                    //this item doesn't exist, error message
+                    MessageBox.Show("This item does not exist.");
+                }
+
+                //read in table data
+                LoadData();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //update or change the current item's information
+            string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""C:\Users\Dingo\source\repos\ProfessionalInventoryManager\Professional Inventory Manager\Professional Inventory Manager\DB\Data.mdf"";Integrated Security=True;Connect Timeout=30;";
+            SqlConnection con;
+            using (con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                //check if item record exists
+                var sqlQuery = "";
+
+                //update items
+                if (IfItemExists(con, textBox2.Text))
+                {
+                    //this item does exist, update
+                    sqlQuery = @"UPDATE [Item] SET [ItemName] = '" + textBox3.Text + "' ,[ItemQuantity] = '" + textBox4.Text + "' ,[ItemPrice] = '" + textBox5.Text + "' ,[ItemShippingDate] = '" + textBox6.Text + "' WHERE [ItemID] = '" + textBox2.Text + "'";
+                }
+                else
+                {
+                    //this item doesn't exist, insert
+                    sqlQuery = @"INSERT INTO[dbo].[Item]
+                    ([ItemID]
+                    ,[ItemName]
+                    ,[ItemQuantity]
+                    ,[ItemPrice]
+                    ,[ItemShippingDate])
+                    VALUES
+                    ('" + textBox2.Text + "', '" + textBox3.Text + "', '" + textBox4.Text + "', '" + textBox5.Text + "', '" + textBox6.Text + "')";
+                }
+
+                SqlCommand cmd = new SqlCommand(sqlQuery, con);
+                cmd.ExecuteNonQuery();
+                con.Close();
+
+                //read in table data
+                LoadData();
+            }
         }
     }
 }
